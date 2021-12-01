bb36abc25a15f0f0c6e63b95a49cd23d3a68d6f0
Jesse Cui <cuijesse@gmail.com>
2021-12-01 15:36:19 -0500

    Insert or update scores upon queueing and deleting questions

--- server/src/resolvers/questionReview.ts
+++ server/src/resolvers/questionReview.ts
@@ -12,6 +12,8 @@ import {
 import { getConnection } from "typeorm";
 import { Question } from "../entities/Question";
 import { QuestionReview, ReviewStatus } from "../entities/QuestionReview";
+import { QuestionSubject } from "../entities/QuestionSubject";
+import { Score } from "../entities/Score";
 import { isAuth } from "../middleware/isAuth";
 import { MyContext } from "../types";
 
@@ -26,14 +28,14 @@ export class QuestionReviewResolver {
   async questionReview(
     @Arg("questionId", () => Int) questionId: number,
     @Ctx() { req }: MyContext
-  ): Promise<QuestionReview | undefined> {    
+  ): Promise<QuestionReview | undefined> {
     const questionReview = await QuestionReview.findOne({
       where: {
         questionId,
         userId: req.session.userId,
       },
     });
-    return  questionReview;
+    return questionReview;
   }
 
   @Mutation(() => QuestionReview)
@@ -48,25 +50,75 @@ export class QuestionReviewResolver {
 
     let afterTomorrow = new Date();
     afterTomorrow.setUTCDate(today.getUTCDate() + 2);
-    const review = await getConnection()
-      .createQueryBuilder()
-      .insert()
-      .into(QuestionReview)
-      .values({
-        userId: req.session.userId,
-        questionId,
-        reviewStatus,
-        correctStreak: reviewStatus == ReviewStatus.CORRECT ? 1 : 0,
-        dateNextAvailable:
-          reviewStatus == ReviewStatus.INCORRECT
-            ? new Date(tomorrow)
-            : reviewStatus == ReviewStatus.CORRECT
-            ? new Date(afterTomorrow)
-            : today,
-      })
-      .returning("*")
-      .execute();
-    return review.raw[0];
+    let review = new QuestionReview();
+    await getConnection().transaction(async (manager) => {
+      const rawReview = await manager
+        .createQueryBuilder()
+        .insert()
+        .into(QuestionReview)
+        .values({
+          userId: req.session.userId,
+          questionId,
+          reviewStatus,
+          correctStreak: reviewStatus == ReviewStatus.CORRECT ? 1 : 0,
+          dateNextAvailable:
+            reviewStatus == ReviewStatus.INCORRECT
+              ? new Date(tomorrow)
+              : reviewStatus == ReviewStatus.CORRECT
+              ? new Date(afterTomorrow)
+              : today,
+        })
+        .returning("*")
+        .execute();
+      review = rawReview.raw[0];
+
+      const questionSubjects = await QuestionSubject.find({
+        where: { questionId },
+        skip: 0,
+        take: 10,
+      });
+      const subjects = questionSubjects.map((subject) => subject.subjectName);      
+      if (subjects) {
+        await Promise.all(
+          subjects.map(async (subject) => {
+            const existingScore = await Score.findOne({
+              subjectName: subject,
+              userId: req.session.userId,
+            });
+            if (!existingScore) {
+              await manager
+                .createQueryBuilder()
+                .insert()
+                .into(Score)
+                .values({
+                  subjectName: subject,
+                  userId: req.session.userId,
+                  queued: reviewStatus == ReviewStatus.QUEUED ? 1 : 0,
+                  correct: reviewStatus == ReviewStatus.CORRECT ? 1 : 0,
+                  incorrect: reviewStatus == ReviewStatus.INCORRECT ? 1 : 0,
+                })
+                .execute();
+            } else {
+              await manager
+                .createQueryBuilder()
+                .update(Score)
+                .where({subjectName: subject, userId: req.session.userId})
+                .set(
+                  reviewStatus == ReviewStatus.QUEUED
+                    ? { queued: () => "queued + 1" }
+                    : reviewStatus == ReviewStatus.CORRECT
+                    ? { correct: () => "correct + 1" }
+                    : reviewStatus == ReviewStatus.INCORRECT
+                    ? { incorrect: () => "incorrect + 1" }
+                    : {}
+                )
+                .execute();
+            }
+          })
+        );
+      }
+    });
+    return review;
   }
 
   @Mutation(() => QuestionReview, { nullable: true })
@@ -91,12 +143,12 @@ export class QuestionReviewResolver {
         correctStreak = existingReview.correctStreak + 1;
         dateNextAvailable.setDate(
           dateNextAvailable.getDate() + correctStreak * 2
-        );        
+        );
       } else if (reviewStatus == ReviewStatus.INCORRECT) {
         correctStreak = 0;
         dateNextAvailable.setDate(
           dateNextAvailable.getDate() + correctStreak + 1
-        );        
+        );
       } else {
         return null;
       }
@@ -126,7 +178,92 @@ export class QuestionReviewResolver {
     @Arg("questionId", () => Int) questionId: number,
     @Ctx() { req }: MyContext
   ): Promise<boolean> {
-    await QuestionReview.delete({ userId: req.session.userId, questionId });
+    const questionReview = await QuestionReview.findOne({
+      userId: req.session.userId,
+      questionId,
+    });
+
+    await getConnection().transaction(async (manager) => {
+      await manager
+        .createQueryBuilder()
+        .delete()
+        .from(QuestionReview)
+        .where({ userId: req.session.userId, questionId })
+        .execute();
+
+        const questionSubjects = await QuestionSubject.find({
+          where: { questionId },
+          skip: 0,
+          take: 10,
+        });
+        const subjects = questionSubjects.map((subject) => subject.subjectName);
+
+      if (subjects) {
+        await Promise.all(
+          subjects.map(async (subject) => {
+            // Check the review status
+            const reviewStatus = questionReview?.reviewStatus;
+
+            // Check if existing correct score column is > 0
+            const score = await Score.findOne({
+              subjectName: subject,
+              userId: req.session.userId,
+            });
+
+            if (score) {
+              const key =
+                reviewStatus == ReviewStatus.QUEUED
+                  ? "queued"
+                  : reviewStatus == ReviewStatus.CORRECT
+                  ? "correct"
+                  : reviewStatus == ReviewStatus.INCORRECT
+                  ? "incorrect"
+                  : "";
+
+              if (key) {
+                const existingScoreValue = score[key];
+                if (existingScoreValue > 0) {                  
+                  // If so, decremement column
+                  const newScoreResult = await manager
+                    .createQueryBuilder()
+                    .update(Score)
+                    .where({subjectName: subject, userId: req.session.userId})
+                    .set(
+                      reviewStatus == ReviewStatus.QUEUED
+                        ? { queued: () => "queued - 1" }
+                        : reviewStatus == ReviewStatus.CORRECT
+                        ? { correct: () => "correct - 1" }
+                        : reviewStatus == ReviewStatus.INCORRECT
+                        ? { incorrect: () => "incorrect - 1" }
+                        : {}
+                    )
+                    .returning("*")
+                    .execute();
+
+                  // Check if all scores are 0. If so, delete the score.
+                  const newScore = newScoreResult.raw[0] as Score;
+                  if (
+                    newScore.queued <= 0 &&
+                    newScore.correct <= 0 &&
+                    newScore.incorrect <= 0
+                  ) {
+                    await manager
+                      .createQueryBuilder()
+                      .delete()
+                      .from(Score)
+                      .where({
+                        subjectName: subject,
+                        userId: req.session.userId,
+                      })
+                      .execute();
+                  }
+                }
+              }
+            }
+          })
+        );
+      }
+    });
     return true;
   }
 }
