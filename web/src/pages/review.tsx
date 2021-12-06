e695ede2a0104c9bb3e780e4fceffcc5643ba6c8
Jesse Cui <cuijesse@gmail.com>
2021-12-06 17:49:04 -0600

    Fix no questions for review logic

--- web/src/pages/review.tsx
+++ web/src/pages/review.tsx
@@ -1,31 +1,22 @@
 import { Box } from "@chakra-ui/layout";
-import {
-  Skeleton,
-  SkeletonCircle,
-  SkeletonText,
-  Stack,
-  Text,
-} from "@chakra-ui/react";
+import { SkeletonCircle, SkeletonText, Stack, Text } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import React from "react";
-import { useMeQuery } from "../generated/graphql";
+import { QuestionReview, useMeQuery } from "../generated/graphql";
 import { withApollo } from "../utils/withApollo";
 
 const Review: React.FC<{}> = ({}) => {
   const router = useRouter();
   const { data: meData, loading: meLoading } = useMeQuery();
-
+  let reviewableQuestions: QuestionReview[] = [];
   if (meData?.me?.questionReviews.length) {
-    const reviewableQuestions = meData?.me?.questionReviews.filter(
+    reviewableQuestions = meData?.me?.questionReviews.filter(
       (review) =>
         new Date().getTime() >= new Date(review.dateNextAvailable).getTime()
-    );
-    if (reviewableQuestions?.length) {
-      router.push("/review/" + reviewableQuestions[0].questionId);
-      
-    } else if (meData.me.questionReviews.length) {
-      router.push("/review/" + meData.me.questionReviews[0].questionId);      
-    }
+    ) as QuestionReview[];
+  }
+  if (reviewableQuestions.length) {
+    router.push("/review/" + reviewableQuestions[0].questionId);
     return (
       <Stack>
         <SkeletonCircle size="10" />
