898904bc80cfed877c8c3db6615a6a3893c64ee9
Jesse Cui <cuijesse@gmail.com>
2022-02-03 17:39:19 -0500

    Add website title and fix avatar photo formatting

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -1,20 +1,25 @@
-import { ColorModeScript } from '@chakra-ui/color-mode'
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
-import theme from '../theme'
+import { ColorModeScript } from "@chakra-ui/color-mode";
+import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+import theme from "../theme";
 
 export default class Document extends NextDocument {
   render() {
     return (
       <Html>
         <Head>
-        <link rel="shortcut icon" href="/favicon.ico?v=1"  type="image/x-icon" />
-          </Head>
+          <title>Schoolyard</title>
+          <link
+            rel="shortcut icon"
+            href="/favicon.ico?v=1"
+            type="image/x-icon"
+          />
+        </Head>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-        <body>          
+        <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
